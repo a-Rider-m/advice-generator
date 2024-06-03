c4708a0b8094c1d53b4269d618b0704a87d7055c
Miguel Angel Arias <miguel.a.frontend@gmail.com>
2024-06-03 13:56:08 -0500

    fetchData function and randomAdvice function

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const adviceId = document.querySelector('#advice-id');
+const adviceContainer = document.querySelector('#advice-container');
+const button = document.querySelector('#button');
+
+const URL = 'https://api.adviceslip.com/advice';
+
+button.addEventListener('click', randomAdvice);
+
+async function fetchData(url) {
+    const response = await fetch(url)
+    const data = await response.json();
+
+    return data;
+};
+
+async function randomAdvice() {
+    const data = await fetchData(URL);
+
+    adviceId.innerText = `ADVICE #${data.slip.id}`;
+    adviceContainer.innerText = `${data.slip.advice}`;
+};
